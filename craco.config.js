1fba071876da53ada47ef9286b4e63e48b8d4e45
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-02-01 11:47:36 +0500

    'hel'

--- craco.config.js
+++ craco.config.js
@@ -23,7 +23,7 @@ module.exports = {
                             '@disabled-color': 'rgba(0, 0, 0, 0.25)', // disable state color
                             '@border-radius-base': '2px', // major border radius
                             '@border-color-base': '#d9d9d9',
-                            ' @box-shadow-base': '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)'
+                            '@box-shadow-base': '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)'
                         },
                         javascriptEnabled: true
                     }
