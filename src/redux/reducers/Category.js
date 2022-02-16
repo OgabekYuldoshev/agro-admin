3b9eb11985a57dfbd401fb8c8e6df271bf040c88
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-02-17 02:38:34 +0500

    commit

--- src/redux/reducers/Category.js
+++ src/redux/reducers/Category.js
@@ -1,89 +1,105 @@
 // ** Redux Imports
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { http } from "@utils"
-import { message } from 'antd'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { http } from "@utils";
+import { message } from "antd";
 
-export const getCategory = createAsyncThunk('app/getCategory', async () => {
-    const response = await http.get('/admin/categories')
-    return response.data?.data
-})
+export const getCategory = createAsyncThunk("app/getCategory", async () => {
+  const response = await http.get("/admin/categories");
+  return response.data?.data;
+});
 
-export const createCategory = createAsyncThunk('app/createCategory', async (data, { dispatch }) => {
-    const response = await http.post('/admin/categories', data)
-    if (response.status === 201) dispatch(getCategory())
-    return response.data
-})
+export const createCategory = createAsyncThunk(
+  "app/createCategory",
+  async (data, { dispatch }) => {
+    const response = await http.post("/admin/categories", data);
+    if (response.status === 201) dispatch(getCategory());
+    return response.data;
+  }
+);
 
-export const deleteCategory = createAsyncThunk('app/deleteCategory', async ({ id }, { dispatch }) => {
-    const response = await http.delete(`/admin/categories/${id}`)
-    if (response.status === 200) dispatch(getCategory())
+export const deleteCategory = createAsyncThunk(
+  "app/deleteCategory",
+  async ({ id }, { dispatch }) => {
+    const response = await http.delete(`/admin/categories/${id}`);
+    if (response.status === 200) dispatch(getCategory());
+  }
+);
 
-})
+export const updateCategory = createAsyncThunk(
+  "app/updateCategory",
+  async ({ id, value }, { dispatch }) => {
+    const response = await http.put(`/admin/categories/${id}`, value);
+    if (response.status === 200) dispatch(getCategory());
+    return response.data;
+  }
+);
 
-export const updateCategory = createAsyncThunk('app/updateCategory', async ({ id, value }, { dispatch }) => {
-    const response = await http.put(`/admin/categories/${id}`, value)
-    if (response.status === 200) dispatch(getCategory())
-    return response.data
-})
+export const createSubCategory = createAsyncThunk(
+  "app/createSubCategory",
+  async (data) => {
+    const response = await http.post("/admin/sub_categories", data);
+    return response.data;
+  }
+);
 
-export const createSubCategory = createAsyncThunk('app/createSubCategory', async (data) => {
-    const response = await http.post('/admin/sub_categories', data)
-    return response.data
-})
+export const deleteSubCategory = createAsyncThunk(
+  "app/deleteSubCategory",
+  async ({ id }) => {
+    await http.delete(`/admin/sub_categories/${id}`);
+  }
+);
 
-export const deleteSubCategory = createAsyncThunk('app/deleteSubCategory', async ({ id }) => {
-    await http.delete(`/admin/sub_categories/${id}`)
-})
-
-export const updateSubCategory = createAsyncThunk('app/updateSubCategory', async ({ id, value }) => {
-    await http.put(`/admin/sub_categories/${id}`, value)
-    return response.data
-})
-
+export const updateSubCategory = createAsyncThunk(
+  "app/updateSubCategory",
+  async ({ id, value }) => {
+    const response = await http.put(`/admin/sub_categories/${id}`, value);
+    return response.data;
+  }
+);
 
 export const categorySlice = createSlice({
-    name: 'category',
-    initialState: {
-        categories: []
+  name: "category",
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+    // handleSearchQuery: (state, action) => {
+    //     state.query = action.payload
+    // }
+  },
+  extraReducers: {
+    [getCategory.pending]: (state) => {
+      state.isLoading = true;
     },
-    reducers: {
-        // handleSearchQuery: (state, action) => {
-        //     state.query = action.payload
-        // }
+    [getCategory.fulfilled]: (state, action) => {
+      state.categories = action?.payload;
+      state.isLoading = false;
     },
-    extraReducers: {
-        [getCategory.pending]: (state) => {
-            state.isLoading = true
-        },
-        [getCategory.fulfilled]: (state, action) => {
-            state.categories = action?.payload
-            state.isLoading = false
-        },
-        [getCategory.rejected]: () => {
-            state.isLoading = false
-            message.error("Serverda xatolik!")
-        },
-        [createCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [updateCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [deleteCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [createSubCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [deleteSubCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [updateSubCategory.rejected]: () => {
-            message.error("Serverda xatolik!")
-        }
-    }
-})
+    [getCategory.rejected]: (state) => {
+      state.isLoading = false;
+      message.error("Serverda xatolik!");
+    },
+    [createCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [updateCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [deleteCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [createSubCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [deleteSubCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [updateSubCategory.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+  },
+});
 
-export const { } = categorySlice.actions
+export const {} = categorySlice.actions;
 
-export default categorySlice.reducer
+export default categorySlice.reducer;
--- src/redux/reducers/Products.js
+++ src/redux/reducers/Products.js
@@ -1,91 +1,96 @@
 // ** Redux Imports
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { http } from "@utils"
-import { message } from "antd"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { http } from "@utils";
+import { message } from "antd";
 // import axios from 'axios'
 
-export const getProducts = createAsyncThunk('app/getProducts', async (page) => {
-    const response = await http.get(`/admin/products?page=${page}`)
-    return response.data?.data
-})
+export const getProducts = createAsyncThunk("app/getProducts", async (page) => {
+  const response = await http.get(`/admin/products?page=${page}`);
+  return response.data?.data;
+});
 
-export const createProduct = createAsyncThunk('app/createProducts', async (data, { dispatch }) => {
-    const response = await http.post('/admin/products', data, {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    })
-    if (response.status === 201) dispatch(getProducts())
-    return response.data
-})
+export const createProduct = createAsyncThunk(
+  "app/createProducts",
+  async (data, { dispatch }) => {
+    const response = await http.post("/admin/products", data, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    if (response.status === 201) dispatch(getProducts());
+    return response.data;
+  }
+);
 
-export const deleteProduct = createAsyncThunk('app/deleteProduct', async (id, { dispatch }) => {
-    const response = await http.delete(`/admin/products/${id}`)
-    if (response.status === 200) dispatch(getProducts())
-})
+export const deleteProduct = createAsyncThunk(
+  "app/deleteProduct",
+  async (id, { dispatch }) => {
+    const response = await http.delete(`/admin/products/${id}`);
+    if (response.status === 200) dispatch(getProducts());
+  }
+);
 
 // export const updatePartner = createAsyncThunk('app/updatePartner', async ({ id, value }) => {
 //     await http.put(`/admin/partners/${id}`, value)
 //     return response.data
 // })
 
-
 export const productsSlice = createSlice({
-    name: 'products',
-    initialState: {
-        products: [],
-        isLoading: false,
-        total: 0,
-        per_page: 0,
-        current_page: 0
-    },
-    reducers: {
-        // handleSearchQuery: (state, action) => {
-        //     state.query = action.payload
-        // }
-    },
-    extraReducers: {
-        [getProducts.pending]: (state) => {
-            state.isLoading = true
-        },
-        [getProducts.fulfilled]: (state, action) => {
-            state.products = action?.payload?.data
-            state.isLoading = false
-            state.total = action?.payload?.total
-            state.per_page = action?.payload?.per_page
-            state.current_page = action?.payload?.current_page
-        },
-        [getProducts.rejected]: () => {
-            state.isLoading = false
-            message.error("Serverda xatolik!")
-        },
-        [createProduct.fulfilled]: () => {
-            message.success("Mahsulot yaratildi!")
-        },
-        [createProduct.rejected]: () => {
-            message.error("Serverda xatolik!")
-        },
-        [deleteProduct.fulfilled]: () => {
-            message.success("Mahsulot o'chrildi!")
-        },
-        [deleteProduct.rejected]: () => {
-            message.error("Serverda xatolik!")
-        }
-        // [deletePartner.rejected]: () => {
-        //     message.error("Serverda xatolik!")
-        // },
-        // [updatePartner.rejected]: () => {
-        //     message.error("Serverda xatolik!")
-        // }
-    }
-    // extraReducers: builder => {
-    //     builder
-    //         .addCase(getCategory.fulfilled, (state, action) => {
-    //             state.categories = action?.payload
-    //         })
+  name: "products",
+  initialState: {
+    products: [],
+    isLoading: false,
+    total: 0,
+    per_page: 0,
+    current_page: 0,
+  },
+  reducers: {
+    // handleSearchQuery: (state, action) => {
+    //     state.query = action.payload
     // }
-})
+  },
+  extraReducers: {
+    [getProducts.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getProducts.fulfilled]: (state, action) => {
+      state.products = action?.payload?.data;
+      state.isLoading = false;
+      state.total = action?.payload?.total;
+      state.per_page = action?.payload?.per_page;
+      state.current_page = action?.payload?.current_page;
+    },
+    [getProducts.rejected]: (state) => {
+      state.isLoading = false;
+      message.error("Serverda xatolik!");
+    },
+    [createProduct.fulfilled]: () => {
+      message.success("Mahsulot yaratildi!");
+    },
+    [createProduct.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    [deleteProduct.fulfilled]: () => {
+      message.success("Mahsulot o'chrildi!");
+    },
+    [deleteProduct.rejected]: () => {
+      message.error("Serverda xatolik!");
+    },
+    // [deletePartner.rejected]: () => {
+    //     message.error("Serverda xatolik!")
+    // },
+    // [updatePartner.rejected]: () => {
+    //     message.error("Serverda xatolik!")
+    // }
+  },
+  // extraReducers: builder => {
+  //     builder
+  //         .addCase(getCategory.fulfilled, (state, action) => {
+  //             state.categories = action?.payload
+  //         })
+  // }
+});
 
-export const { } = productsSlice.actions
+export const {} = productsSlice.actions;
 
-export default productsSlice.reducer
+export default productsSlice.reducer;
